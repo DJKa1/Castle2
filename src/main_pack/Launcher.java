54d56c0131908f5e9ccbecaad8e4a5719b17a12a
Pete <pete.benz@topmac.de>
2020-04-02 17:43:47 +0200

    Huge Error fix

--- src/main_pack/Launcher.java
+++ src/main_pack/Launcher.java
@@ -3,7 +3,7 @@ package main_pack;
 import java.awt.*;
 
 public class Launcher {
-    public static int WIDTH =1024, HEIGHT =1024;
+    public static int WIDTH =1224, HEIGHT =1224;
     public static String VERSION ="0.0.1";
 
     public static void main(String[] args) {
