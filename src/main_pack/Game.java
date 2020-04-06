b5e429306bffd2149fc5ddbcaee08c12378e5802
Pete <pete.benz@topmac.de>
2020-04-06 14:26:02 +0200

    Level

--- src/main_pack/Game.java
+++ src/main_pack/Game.java
@@ -59,7 +59,7 @@ public class Game implements Runnable {
 
         projectileHandler=new ProjectileHandler();
         player=new Player(1,3,projectileHandler,creatureHandler);
-        map = new Map("FirstLevel");
+        map = new Map("Level");
         gameState=new GameState(this);
 
         consoleState=new ConsoleState(this);
